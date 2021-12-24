a9f70af4504fc411094784edd771087a9ddc8ae8
Alexandru-Valentin Harapu <aharapu2@yahoo.com>
2021-12-24 21:26:41 +0200

    Traverse contents of arrow function block statement

--- bin/code-file.js
+++ bin/code-file.js
@@ -1,23 +1,29 @@
-function motherFunc() {
-  const a = 1;
+// function motherFunc() {
+//   const a = 1;
 
-  function firstChildFunc() {
-    let b = 2;
+//   function firstChildFunc() {
+//     let b = 2;
+//   }
+
+//   const secondChildFunc = (arg1) => {
+//     console.log(arg1);
+//   };
+// }
+
+// function reactComponent(props) {
+//   const { prop } = props;
+
+//   return <div>some html content</div>;
+// }
+
+// function addTwoNumbers(a, b) {
+//   return a + b;
+// }
+
+const arrowFuncWithClosure = (n) => {
+  function insideArrowFunction() {
+    return 'olaola'
   }
 
-  const secondChildFunc = (arg1) => {
-    console.log(arg1);
-  };
+  return n*n;
 }
-
-function reactComponent(props) {
-  const { prop } = props;
-
-  return <div>some html content</div>;
-}
-
-function addTwoNumbers(a, b) {
-  return a + b;
-}
-
-const arrowFuncWithoutClosure = (n) => n*n;
--- bin/index.js
+++ bin/index.js
@@ -7,8 +7,9 @@ let tabSize = 2; // default tab size
 
 // TODO -> install packages for colors and decoration (chalk boxen)
 
-const fileName = process.argv[2] === 'runner' ? 'code-file.js' : process.argv[2]; // default file name present for Quokka to work during development
+const fileName = process.argv[2] === 'runner' ? './bin/code-file.js' : process.argv[2]; // default file name present for Quokka to work during development
 // TODO -> a validator will be needed that a file is passed
+// TODO -> use this: console.log('current working directory: ', process.cwd());
 console.log('file name ', fileName);
 
 const executionArguments = process.argv.slice(2);
@@ -19,7 +20,7 @@ executionArguments.forEach(argument => {
     case 'tabSize':
       tabSize = parseInt(value, 10);
       break;
-    // TODO -> add an option to use spaces instead of tabs
+    // TODO -> add an option to use tabs instead of spaces
     default:
       break;
   }
@@ -29,10 +30,13 @@ const buffer = fs.readFileSync(fileName);
 const code = buffer.toString();
 
 const syntaxTree = babelParser.parse(code, { plugins: ['jsx']});
+const declaration = syntaxTree.program.body[0].declarations[0];
+const body = declaration.init.body;
+body;
 
 let testFileString = '';
 
-let tabs = 0; // will increase to 0 when the recursion intiates
+let tabs = 0;
 let spaces = '';
 
 function increaseSpaces() {
@@ -48,6 +52,8 @@ function decreaseSpaces() {
 }
 
 // TODO -> check if an export statement is correctly parsed
+// TODO -> recursion should enter if / else blocks as well as switch statements
+// TODO -> should the code check inside IIFEs?
 function checkNode(node) {
 
   // TODO -> add a case when it's a variable declaration, to check if it is an arrow function declaration.
@@ -58,24 +64,12 @@ function checkNode(node) {
     case 'VariableDeclaration':
       const declaration = node.declarations[0]; // assuming only 1 declaration. a limitation which can get extended.
       if (declaration.init.type === 'ArrowFunctionExpression') {
-        const body = declaration.init.body;
-        if (body.type === 'JSXElement') {
-          // TODO -> write component specific test
-        } else {
-          testFileString += spaces + `describe('${declaration.id.name}', () => {\n`;
-          // TODO -> if it has a body, iterate and check its elements
-          testFileString += spaces + `});\n`;
-        }
+        addArrowFunctionExpression(declaration);
       }
       break;
 
     case 'FunctionDeclaration':
-      // TODO -> extract to handler function
-      testFileString += spaces + `describe('${node.id.name}', () => {\n`;
-      increaseSpaces();
-      if (node?.body?.body) node.body.body.forEach(childNode => checkNode(childNode));
-      decreaseSpaces();
-      testFileString += spaces + `});\n`;
+      addFunctionDeclaration(node);
       break;
   
     case 'File':
@@ -89,3 +83,27 @@ function checkNode(node) {
 checkNode(syntaxTree);
 
 console.log(testFileString);
+
+function addFunctionDeclaration(node) {
+  testFileString += spaces + `describe('${node.id.name}', () => {\n`;
+  increaseSpaces();
+  if (node?.body?.body) node.body.body.forEach(childNode => checkNode(childNode));
+  decreaseSpaces();
+  testFileString += spaces + `});\n`;
+}
+
+function addArrowFunctionExpression(node) {
+  const body = node.init.body;
+
+  if (body.type === 'JSXElement') {
+    // TODO -> write component specific test
+  } else {
+    testFileString += spaces + `describe('${node.id.name}', () => {\n`;
+    if (body.type === 'BlockStatement') {
+      increaseSpaces();
+      body.body.forEach(childNode => checkNode(childNode));
+      decreaseSpaces();
+    }
+    testFileString += spaces + `});\n`;
+  }
+}
